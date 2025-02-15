6a68226b2c97e84b2ec0f308992cf6fa75eaded5
KauaNdevs <kaua.martins@ndevs.com.br>
2025-02-15 10:31:14 -0300

    Testando estilizaçao no next js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
+import React from "react"
 import Link from "../src/components/Link"
+
+function Title({ children, as }) {
+  const Tag = as
+  return (
+    <React.Fragment>
+      <Tag>
+        {children}
+      </Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: red;
+          font-family: sans-serif
+        }
+        `}</style>
+    </React.Fragment>
+  )
+}
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Desenvti cases - Home</h1>
+      <Title as="h1">Desenvti cases - Home</Title>
       <Link href={"/faq"}>
         Ir para a pagina do faq
       </Link>
